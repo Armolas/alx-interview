283724c68097b6ec6734d18861224e643cfb554e
Armolas <armolas06@gmail.com>
2024-06-07 02:07:21 +0100

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
+'''lockboxes module'''
 from collections import deque
 
 
 def canUnlockAll(boxes):
+    '''returns true if all boxes can be unlocked'''
     n = len(boxes)
     opened = set()
     queue = deque([0])
-    
     while queue:
         current = queue.popleft()
         if current not in opened:
@@ -14,5 +15,4 @@ def canUnlockAll(boxes):
             for key in boxes[current]:
                 if key < n and key not in opened:
                     queue.append(key)
-    
     return len(opened) == n
