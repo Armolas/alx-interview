de637e7aa2f50a47a57355408eba7b1bc76355ef
Armolas <armolas06@gmail.com>
2024-06-07 02:04:48 +0100

    0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from collections import deque
+
+
+def canUnlockAll(boxes):
+    n = len(boxes)
+    opened = set()
+    queue = deque([0])
+    
+    while queue:
+        current = queue.popleft()
+        if current not in opened:
+            opened.add(current)
+            for key in boxes[current]:
+                if key < n and key not in opened:
+                    queue.append(key)
+    
+    return len(opened) == n
