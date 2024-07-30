9d9246c418b6530bec734164a18dfb4244c97435
Armolas <armolas06@gmail.com>
2024-07-30 21:25:23 +0100

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -19,7 +19,7 @@ def island_perimeter(grid):
                     perimeter += 1
                 if j == cols - 1 or grid[i][j + 1] == 0:
                     perimeter += 1
-                if i == rows + 1 or grid[i + 1][j] == 0:
+                if i == rows - 1 or grid[i + 1][j] == 0:
                     perimeter += 1
             j += 1
         i += 1
